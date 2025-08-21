6052097559db9b9f5dbfc4f51fa54366b568345f
AmazinAxel <axel@amazinaxel.com>
2025-08-20 19:05:45 -0700

    feat: remove DND button from ags quicksettings

--- ags/widgets/quicksettings/quicksettings.tsx
+++ ags/widgets/quicksettings/quicksettings.tsx
@@ -1,19 +1,8 @@
 import { BrightnessSlider } from '../../services/brightness';
 import { VolumeSlider, SinkSelector } from './sound';
-import { Astal, Gdk, Gtk } from 'ags/gtk4';
+import { Astal, Gtk } from 'ags/gtk4';
 import app from 'ags/gtk4/app'
-import { DND, setDND } from '../notifications/notifications';
 const { BOTTOM, LEFT } = Astal.WindowAnchor;
-
-const DNDToggle = () =>
-    <button
-        widthRequest={60}
-        cssClasses={DND((dnd) => (dnd) ? ['dnd', 'active'] : ['dnd'])}
-        cursor={Gdk.Cursor.new_from_name('pointer', null)}
-        onClicked={() => setDND(!DND.get())}
-    >
-        <image iconName="notifications-disabled-symbolic"/>
-    </button>
 
 export default () =>
     <window
@@ -21,13 +10,12 @@ export default () =>
         anchor={BOTTOM | LEFT}
         application={app}
     >
-        <box widthRequest={400} cssClasses={['quickSettings']} orientation={Gtk.Orientation.VERTICAL}>
+        <box widthRequest={350} cssClasses={['quickSettings']} orientation={Gtk.Orientation.VERTICAL}>
             <box marginBottom={5}>
                 <box orientation={Gtk.Orientation.VERTICAL}>
                     <VolumeSlider/>
                     <BrightnessSlider/>
                 </box>
-                <DNDToggle/>
             </box>
             <SinkSelector/>
         </box>
