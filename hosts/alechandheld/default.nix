b6e7297bc8e599560739b83e5313ca22e8d57485
AmazinAxel <axel@amazinaxel.com>
2025-10-26 13:52:41 -0700

    feat: add emulationstation test to alechandheld

--- hosts/alechandheld/default.nix
+++ hosts/alechandheld/default.nix
@@ -4,5 +4,16 @@
     ../common.nix
   ];
 
+  environment.systemPackages = with pkgs; [
+    emulationstation
+  ];
+
+  boot.loader = {
+    grub.enable = false;
+    generic-extlinux-compatible.enable = true;
+  };
+
+  services.sshd.enable = true;
+
   networking.hostName = "alecolaptop";
 }
