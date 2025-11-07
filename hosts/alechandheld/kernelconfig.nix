f15c258c5062b0567392e2d590c761f383411842
AmazinAxel <axel@amazinaxel.com>
2025-11-07 09:58:55 -0800

    chore(alechandheld): only include config for proper joystick/gamepad support

--- hosts/alechandheld/kernelconfig.nix
+++ hosts/alechandheld/kernelconfig.nix
@@ -2,21 +2,10 @@
   boot.kernelPatches = [{
     name = "h700-rg35xx-h-config";
     patch = null;
-    #ignoreConfigErrors = true;
     structuredExtraConfig = with lib.kernel; {
-      INPUT = yes;
-      INPUT_EVDEV = yes;
-      KEYBOARD_ADC = yes;
-      INPUT_JOYSTICK = yes;
       JOYSTICK_ADC = yes;
-      #INPUT_MISC = yes;
-      #INPUT_AXP20X_PEK = yes;
-      #INPUT_UINPUT = yes;
-      #INPUT_PWM_VIBRA = yes;
-      USB_EHCI_HCD = yes;
-      SUN20I_GPADC = yes;
       MUX_GPIO = yes;
-      KEYS = yes;
+      IIO_MUX = yes;
     };
   }];
 }
