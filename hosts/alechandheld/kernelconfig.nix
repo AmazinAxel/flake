9240eca803679b00f206fb32e87c5aa60646c80e
AmazinAxel <axel@amazinaxel.com>
2025-11-11 09:50:36 -0800

    fix(alechandheld): fix kernel defconfig (attempt 2)

--- hosts/alechandheld/kernelconfig.nix
+++ hosts/alechandheld/kernelconfig.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, ...}: {
   boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.linux_latest.override {
-    extraConfig = lib.readFile ./config.conf;
+    configfile = ./config.conf;
     ignoreConfigErrors = true;
   }));
 }
