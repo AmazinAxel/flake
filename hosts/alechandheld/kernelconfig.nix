5e0654bf5df552ee94f64c3f38bf3193a5acf22b
AmazinAxel <axel@amazinaxel.com>
2025-11-14 19:20:03 -0800

    fix(alechandheld): fix new kernel config

--- hosts/alechandheld/kernelconfig.nix
+++ hosts/alechandheld/kernelconfig.nix
@@ -4,12 +4,14 @@ let
   kernel = pkgs.linuxManualConfig {
     inherit (pkgs) stdenv lib;
     version = "6.16.9";
-    configfile = ./kernel.config;
     src = pkgs.fetchurl {
       url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.16.9.tar.xz";
-      sha256 = "sha256-13g59jvc6kvp8dzl6ysmzrpq4nh9xvy5va5avrsn6iq5ryiwij3s";
+      sha256 = "13g59jvc6kvp8dzl6ysmzrpq4nh9xvy5va5avrsn6iq5ryiwij3s";
     };
+    configfile = ./kernel.config;
   };
+
+  h700KernelPackage = pkgs.linuxPackagesFor kernel;
 in {
-  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor kernel);
+  boot.kernelPackages = lib.mkForce h700KernelPackage;
 }
