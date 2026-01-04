6821a79083271aa7b3a475a148bf088a4d1eea17
AmazinAxel <axel@amazinaxel.com>
2026-01-03 21:36:43 -0800

    chore: update librewolf start workspace on alecolaptop host

--- hosts/alecolaptop/hm.nix
+++ hosts/alecolaptop/hm.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [ # Autostart apps
-      "[workspace 3 silent] librewolf"
+      "[workspace 1 silent] librewolf"
     ];
   
     bind = [ "Super, D, exec, screenshot" ];# Custom side mouse key for quick screenshots
