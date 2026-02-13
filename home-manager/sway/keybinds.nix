7a2ae66eac43518700cc874008bbaaa52003d17a
AmazinAxel <axel@amazinaxel.com>
2026-02-13 11:31:50 -0800

    chore: keybind descriptions

--- home-manager/sway/keybinds.nix
+++ home-manager/sway/keybinds.nix
@@ -15,52 +15,59 @@ in {
       "XF86MonBrightnessUp" = "exec brightnessctl set +15%";
       "XF86MonBrightnessDown" = "exec brightnessctl set -15%";
 
-      # Power menu
+      # Apps
+      "${mod}+E" = "exec librewolf";
+      "${mod}+return" = "exec foot";
+      
+      # Ags
+      "${mod}+Z" = "exec ags toggle bar";
+      "${mod}+space" = "exec ags toggle launcher";
+      "${mod}+period" = "exec ags toggle emojiPicker";
+      "${mod}+X" = "exec ags toggle quickSettings";
+      "${mod}+A" = "exec ags toggle chat";
+      "${mod}+C" = "exec ags request hideNotif"; # Closes last notification
+      "${mod}+control+D" = "exec ags request toggleDND";
+
+      "${mod}+V" = "exec ags toggle clipboard";
+      # Enter = select entry, C = copy 2nd entry, E = edit image with swappy, W = wipe clipboard
+
+      "${mod}+R" = "exec ags request record";
+      # R = toggle mic, Q = toggle quality, C = clip last 30s, Space = record
+
       "XF86PowerOff" = "exec ags toggle powermenu";
-      "${mod}+shift+S" = "exec toggle powermenu";
+      "${mod}+shift+S" = "exec ags toggle powermenu";
+      # S = sleep, Q = shutdown, L = lock, R = restart 
 
-      "${mod}+E" = "exec librewolf"; # Browser
-      "${mod}+return" = "exec foot"; # Terminal
-      "${mod}+Z" = "exec ags toggle bar"; # Show/hide bar
-      "${mod}+V" = "exec ags toggle clipboard"; # Clipboard
-      "${mod}+space" = "exec ags toggle launcher"; # App laucher
-      "${mod}+period" = "exec ags toggle emojiPicker"; # Emoji picker
-      "${mod}+X" = "exec ags toggle quickSettings"; # Quicksettings
-      "${mod}+A" = "exec ags toggle chat"; # Chat
-      "${mod}+C" = "exec ags request hideNotif"; # Clear last notification
-      "${mod}+R" = "exec ags request record"; # Clear last notification
-      "${mod}+control+D" = "exec ags request toggleDND"; # Clear last notification
-
-      "${mod}+control+period" = "exec ags request 'media next'"; # Next mpd song
-      "${mod}+control+comma" = "exec ags request 'media prev'"; # Previous mpd song
-      "${mod}+greater" = "exec ags request 'media nextPlaylist'"; # Next mpd playlist (Shift+.)
-      "${mod}+less" = "exec ags request 'media prevPlaylist'"; # Previous mpd playlist (Shift+,)
-      "${mod}+slash" = "exec ags request 'media toggle'"; # Toggle song status
+      # Ags MPD controls
+      "${mod}+control+period" = "exec ags request 'media next'";
+      "${mod}+control+comma" = "exec ags request 'media prev'";
+      "${mod}+greater" = "exec ags request 'media nextPlaylist'";
+      "${mod}+less" = "exec ags request 'media prevPlaylist'";
+      "${mod}+slash" = "exec ags request 'media toggle'";
 
       "Print" = ''exec wayshot -s "$(slurp)" --stdout | wl-copy''; # Screenshot
 
-      # Focus
+      # Move focus in a workspace
       "${mod}+left" = "focus left";
       "${mod}+right" = "focus right";
       "${mod}+up" = "focus up";
       "${mod}+down" = "focus down";
 
-      # Window positioning
+      # Move window in a workspace
       "${mod}+shift+left" = "move left";
       "${mod}+shift+right" = "move right";
       "${mod}+shift+up" = "move up";
       "${mod}+shift+down" = "move down";
 
-      # Workspaces
+      # Move through workspaces
       "control+${mod}+right" = workspace "+";
       "control+${mod}+left" = workspace "-";
       "control+${mod}+shift+right" = moveItemToWorkspace "+";
       "control+${mod}+shift+left" = moveItemToWorkspace "-";
 
-      "${mod}+shift+F" = "togglefloating"; # Make window non-tiling
-      "F11" = "fullscreen toggle"; # Fullscreen
-      "${mod}+Q" = "kill"; # Close window
-
+      "${mod}+F" = "floating toggle";
+      "F11" = "fullscreen toggle";
+      "${mod}+Q" = "kill";
       "${mod}+tab" = "workspace back_and_forth";
     };
     # Scroll through workspaces
