5f52680426ad724fb98a8fc4f0dadde78baf93e4
AmazinAxel <axel@amazinaxel.com>
2026-03-05 13:42:41 -0800

    fix: no middle mouse paste in librewolf this breaks some webapps

--- home-manager/librewolf.nix
+++ home-manager/librewolf.nix
@@ -4,10 +4,11 @@
 
     # Other settings are managed by Mozilla settings sync
     settings = {
-      "identity.fxaccounts.enabled" = true; # Mozilla account sync
+      "identity.fxaccounts.enabled" = true;
       "toolkit.tabbox.switchByScrolling" = true; # Scroll through tabs
       "middlemouse.contentLoadURL" = true; # Open links in new tab using middle click
       "mousewheel.min_line_scroll_amount" = 30; # Longer scrolls
+      "middlemouse.paste" = false;
     };
   };
 }
