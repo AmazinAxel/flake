e3e6df41647803eee4aa927a7da8c59c4d6aa9b1
AmazinAxel <axel@amazinaxel.com>
2026-04-03 12:41:31 -0700

    feat(zen): mod config/rice zen

--- home-manager/zen.nix
+++ home-manager/zen.nix
@@ -22,6 +22,52 @@
         # hide new tab clutter
         "browser.newtabpage.activity-stream.feeds.system.topsites" = false;
         "browser.newtabpage.activity-stream.feeds.system.topstories" = false;
+
+        # MOD THEMING
+        "uc.essentials.width" = "Normal"; # thin normal wide
+        "uc.essentials.gap" = "Normal"; # small normal big
+        "uc.essentials.same-height" = true;
+        "uc.superpins.border" = "pins";
+        "uc.remove-sidebar-scrollbar" = true;
+        "uc.tabs.show-separator" = "never";
+        "uc.tabs.dim-type" = "both";
+        "uc.pinned.height" = "small";
+        "uc.favicon.size" = "normal"; # large
+        "uc.workspace.icon.size" = "large";
+        "uc.workspace.current.icon.size" = "large";
+
+        "theme.better_find_bar.transparent_background" = true;
+        "theme-better_find_bar-hide_match_case" = "hide_immediately";
+        "theme-better_find_bar-hide_match_diacritics" = "hide_immediately";
+        "theme-better_find_bar-hide_whole_words" = "hide_immediately";
+        "theme.better_find_bar.hide_find_status" = true;
+
+        "uc.fixcontext.ergonomicsfortabs" = true;
+        "uc.hidecontext.separators" = true;
+        "uc.hidecontext.icons" = true;
+        "uc.fixcontext.applyzenaccent" = true;
+        "uc.hidecontext.copylink" = true;
+        "uc.hidecontext.bookmark" = true;
+        "uc.hidecontext.mutetab" = true;
+        "uc.hidecontext.newcontainer" = true;
+        "uc.hidecontext.sendtodevice" = true;
+        "uc.hidecontext.closetab" = true;
+        "uc.hidecontext.askchatbot" = true;
+        "uc.hidecontext.search" = true;
+        "uc.hidecontext.searchinpriv" = true;
+        "uc.hidecontext.translate" = true;
+        "uc.hidecontext.printselection" = true;
+        "uc.hidecontext.image" = true;
+        "uc.hidecontext.audiovideo" = true;
+        "uc.hidecontext.checkspelling" = true;
+        "uc.hidecontext.selectalltext" = true;
+        "uc.hidecontext.reloadtab" = true;
+        "uc.hidecontext.duplicatetab" = true;
+        "uc.hidecontext.savelink" = true;
+        "uc.hidecontext.screenshot" = true;
+        "uc.hidecontext.navigation" = true;
+
+        #"toolkit.legacyUserProfileCustomizations.stylesheets" = true; # custom css
       };
 
       search = {
@@ -42,7 +88,7 @@
       mods = [
         "ad97bb70-0066-4e42-9b5f-173a5e42c6fc" # better pins/essentials
         "a6335949-4465-4b71-926c-4a52d34bc9c0" # better find bar
-        "1e86cf37-a127-4f24-b919-d265b5ce29a0" # remove toolbar clutter
+        #"1e86cf37-a127-4f24-b919-d265b5ce29a0" # remove toolbar clutter
         "81fcd6b3-f014-4796-988f-6c3cb3874db8" # declutter context menu
         "c6813222-6571-4ba6-8faf-58f3343324f6" # remove rounded corners
         "d8b79d4a-6cba-4495-9ff6-d6d30b0e94fe" # better active tab highlight
