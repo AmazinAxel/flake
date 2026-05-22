7a9a191c08d90ebeff7b9a4a404d37cbdabf2c5d
AmazinAxel <axel@amazinaxel.com>
2026-05-22 12:12:47 -0700

    fix(alecdev): fix script permissions

--- hosts/alecdev/scripts.nix
+++ hosts/alecdev/scripts.nix
@@ -10,8 +10,7 @@ in {
         sudo systemctl stop minecraft-server
       fi
 
-      cd ${serverDir}
-      sudo -u minecraft git pull --rebase
+      sudo -u minecraft git -C ${serverDir} pull --rebase
 
       sudo systemctl start minecraft-server
     '')
@@ -23,17 +22,15 @@ in {
         sudo systemctl stop minecraft-server
       fi
 
-      cd ${serverDir}
-
-      sudo -u minecraft git pull --rebase || true
-      sudo -u minecraft git add -A
-      sudo -u minecraft git reset -q -- 'Permafrost' 'Permafrost/*' 'server.properties' 2>/dev/null || true
+      sudo -u minecraft git -C ${serverDir} pull --rebase || true
+      sudo -u minecraft git -C ${serverDir} add -A
+      sudo -u minecraft git -C ${serverDir} reset -q -- 'Permafrost' 'Permafrost/*' 'server.properties' 2>/dev/null || true
 
       # commit message
-      if ! sudo -u minecraft git diff --cached --quiet; then
+      if ! sudo -u minecraft git -C ${serverDir} diff --cached --quiet; then
         date_str=$(date +%-m/%-d/%y)
 
-        top_files=$(sudo -u minecraft git diff --cached --numstat \
+        top_files=$(sudo -u minecraft git -C ${serverDir} diff --cached --numstat \
           | awk '$1 != "-" { print $1+$2"\t"$3 }' \
           | sort -rn \
           | head -3 \
@@ -41,15 +38,15 @@ in {
           | xargs -n1 basename \
           | paste -sd ', ')
 
-        description=$(sudo -u minecraft git diff --cached --numstat \
+        description=$(sudo -u minecraft git -C ${serverDir} diff --cached --numstat \
           | awk '$1 != "-" { printf "%d\t- %s (+%s/-%s)\n", $1+$2, $3, $1, $2 }' \
           | sort -rn \
           | cut -f2-)
 
         msg="feat: sync $date_str $top_files"
 
-        sudo -u minecraft git commit -m "$msg" -m "$description"
-        sudo -u minecraft git push
+        sudo -u minecraft git -C ${serverDir} commit -m "$msg" -m "$description"
+        sudo -u minecraft git -C ${serverDir} push
       else
         echo "[permafrost stop] No major tracked changes"
       fi
