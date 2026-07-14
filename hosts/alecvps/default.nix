d8b7790fa03623cee5fe91823e82e598641e610a
AmazinAxel <87440159+AmazinAxel@users.noreply.github.com>
2026-07-13 22:23:38 -0700

    fix(alecvps): update mc server path

--- hosts/alecvps/default.nix
+++ hosts/alecvps/default.nix
@@ -22,7 +22,7 @@ in {
     fstrim.enable = false; # Proxmox handles this
     minecraft-server = {
       enable = true;
-      dataDir = "/var/lib/mcserver";
+      dataDir = "/var/lib/minecraft";
       package = pkgs.papermc;
       jvmOpts = "-Xmx1567M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -Djava.net.preferIPv6Addresses=true"; # jar is ran with --nogui
       openFirewall = true;
